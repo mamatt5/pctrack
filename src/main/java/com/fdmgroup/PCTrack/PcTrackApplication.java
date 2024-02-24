e6b01c2b8ce53fa82e5c9ffc72fecedb0caa4722
Matthew Chanco <Matthew.Chanco@fdmgroup.com>
2024-02-24 13:40:04 +1100

    Initial commit

---
+++ src/main/java/com/fdmgroup/PCTrack/PcTrackApplication.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.PCTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdmgroup/PCTrack/PcTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmgroup.PCTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
