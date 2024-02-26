6be4de9dcd5f04410704a5e5856fcabeedf07515
Jenny <43059884+IkarosJenny@users.noreply.github.com>
2024-02-26 15:53:57 +1100

    computer service + adding computers as list

--- src/main/java/com/fdmgroup/PCTrack/Dataloader.java
+++ src/main/java/com/fdmgroup/PCTrack/Dataloader.java
@@ -99,6 +99,8 @@ public class Dataloader implements ApplicationRunner {
 		Computer c23 = new Computer(70106, new Room(), null);
 		Computer c24 = new Computer(70162, new Room(), null);
 		
+		List<Computer> computers = Arrays.asList(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11, c12, c13, c14, c15, c16, c17, c18, c19, c20, c21, c22, c23, c24);
+		computerService.saveAll(computers);
 		computerService.save(c1);
 		computerService.save(c2);
 		computerService.save(c3);
