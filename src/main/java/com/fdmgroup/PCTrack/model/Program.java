f58560a5d9484111b03b407dd8e1038dc6641ddb
Jefferson Zheng <jefferson.zheng@fdmgroup.com>
2024-02-27 01:24:44 +0000

    Update Program.java

--- src/main/java/com/fdmgroup/PCTrack/model/Program.java
+++ src/main/java/com/fdmgroup/PCTrack/model/Program.java
@@ -1,47 +1,47 @@
 package com.fdmgroup.PCTrack.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Program {
+	@Id
+	@GeneratedValue
+	@Column(name = "ID")
 	private int programId;
+	@Column(name = "NAME")
 	private String name;
+	@Column(name = "VERSION NO.")
 	private String versionNumber;
 	
-	public Program(int programId, String name, String versionNumber) {
-		super();
-		this.programId = programId;
-		this.name = name;
-		this.versionNumber = versionNumber;
-	}
-	
-	
-
 	public Program(String name, String versionNumber) {
 		super();
 		this.name = name;
 		this.versionNumber = versionNumber;
 	}
-
+	public Program() {
+		super();
+	}
 	public int getProgramId() {
 		return programId;
 	}
-
 	public void setProgramId(int programId) {
 		this.programId = programId;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getVersionNumber() {
 		return versionNumber;
 	}
-
 	public void setVersionNumber(String versionNumber) {
 		this.versionNumber = versionNumber;
 	}
-	
+	@Override
+	public String toString() {
+		return "Program [programId=" + programId + ", name=" + name + ", versionNumber=" + versionNumber + "]";
+	}
 }
+
