d47eed27d7e9611cf26dd4c9f4ee963624496271
Jefferson <68528609+jefferson6263@users.noreply.github.com>
2024-02-27 14:35:44 +1100

    finally in correct repo

---
+++ React Frontend/src/components/LoggedInHomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const LoggedInHomePage = () => {
+  return (
+    <>
+        <div>Normal User Home Page changed</div>
+        <NavBar />
+    </>
+    
+  )
+}
+
+export default LoggedInHomePage
