5435298da76b061ff68e7a3d623bdc2ba21a4adf
Jefferson <68528609+jefferson6263@users.noreply.github.com>
2024-03-01 10:50:47 +1100

    added backend functionality for getting computers in role, frontend receives the data but still need to work on displaying

--- React Frontend/src/components/ViewComputersInRoomPage.jsx
+++ React Frontend/src/components/ViewComputersInRoomPage.jsx
@@ -4,20 +4,27 @@ import { useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { Box } from '@mui/material';
+import callApi from '../api/callApi';
 
 
 const ViewComputersInRoomPage = (props) => {
     const {state} = useLocation();
     const [room, setRoom] = useState({});
-
+    const [computers, setComputers] = useState([])
 
     useEffect(() => {
       setRoom(state)
-    
+
+      const config = {
+        method: "get",
+        endpoint: "getcomputers/"+state["roomId"],
+  
+      }
+
+      callApi(setComputers, null, config);
 	  }, []);
 
-    console.log("please appear")
-    console.log(room)
+    
   return (
 
     <>
