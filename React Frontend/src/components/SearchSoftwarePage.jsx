462a84dc5dd9dade1cc319885aacf7cd3316dc61
Jenny <43059884+IkarosJenny@users.noreply.github.com>
2024-02-29 11:34:49 +1100

    Software table

--- React Frontend/src/components/SearchSoftwarePage.jsx
+++ React Frontend/src/components/SearchSoftwarePage.jsx
@@ -1,14 +1,51 @@
 import React from 'react'
-import NavBar from '../partials/NavBar'
-export const SearchSoftwarePage = () => {
-  return (
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import NavBar from "../partials/NavBar";
+import { useState } from "react";
+import SoftwareTables from "../partials/SoftwareTable";
+import { useEffect } from "react";
+import callApi from "../api/callApi";
 
-    <>
-      <div className='dashBoardPadding'>
-        <h1>Under Construction</h1>
-        <div>SearchSoftwarePage</div>
-        </div>
+const SearchSoftwarePage = () => {
+    const [software, setSoftware] = useState([]);
 
-    </>
-  )
+    useEffect (() => {
+        getSoftware(setSoftware)
+    }, []);
+
+    const getSoftware = (setSoftware) => {
+      // Functionality to get users
+      const config = {
+        method: "get",
+        endpoint: "programs",
+      };
+    
+      // if we cant find the user, its a username issue
+      // if we can, its a password issue.
+      callApi(setSoftware, null, config);
+    };
+    
+    
+    const SoftwareTab = () => {
+      return <></>;
+    };
+
+    return (
+
+      <>
+        {/* <div className='dashBoardPadding'>
+          <h1>Under Construction</h1>
+          <div>SearchSoftwarePage</div>
+        </div> */}
+        <Box className="dashBoardPadding">
+          <Box sx={{ display: "flex", flexDirection: "row" }}>
+                    <SoftwareTab />
+                  </Box>
+                  <SoftwareTables array={software} />
+        </Box>
+
+      </>
+    )
 }
+export default SearchSoftwarePage;
