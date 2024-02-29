32c47d62c0f3d8de7c07bf66f3d41ce6fe9aedc4
Matthew Chanco <Matthew.Chanco@fdmgroup.com>
2024-02-29 18:24:37 +1100

    Mandate can be seen in frontend. Waiting for View Rooms

--- React Frontend/src/components/RoomMandates.jsx
+++ React Frontend/src/components/RoomMandates.jsx
@@ -1,8 +1,36 @@
+import axios from 'axios'
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const RoomMandates = () => {
+  const [roomMandates, setRoomMandates] = useState([])
+  const { roomId } = useParams()
+
+  useEffect(() => {
+    loadMandates()
+  })
+
+
+  const loadMandates = () => {
+    // room 1 for now
+    axios.get(`http://localhost:8181/PCTrack/rooms/3/mandates`)
+    .then(response => {setRoomMandates(response.data)})
+  }
+
   return (
-    <div>RoomMandates</div>
+    <>
+        <div>RoomMandates</div>
+        
+        <div>
+          <ul style={{listStyle: 'none'}}>
+            {roomMandates.map(mandate =>
+              <li>{mandate.description}</li>)}
+          </ul>
+        </div>
+    </>
+
   )
 }
 
