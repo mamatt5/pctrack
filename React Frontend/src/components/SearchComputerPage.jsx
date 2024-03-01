ff61dc3a4319711c7afeb36f763c2c61b43fed2d
Bennett Chung <bennett.chung@fdmgroup.com>
2024-03-01 16:09:36 +1100

    Added room search functionality

--- React Frontend/src/components/SearchComputerPage.jsx
+++ React Frontend/src/components/SearchComputerPage.jsx
@@ -3,7 +3,7 @@ import ComputerCard from '../partials/ComputerCard'
 import { useState } from 'react'
 import { useEffect } from 'react'
 import callApi from '../api/callApi'
-import { Button, InputAdornment, OutlinedInput} from '@mui/material'
+import { Button, InputAdornment, InputLabel, MenuItem, OutlinedInput, Select } from '@mui/material'
 import SearchIcon from "@mui/icons-material/Search";
 import AddComputer from '../partials/AddComputer'
 
@@ -16,26 +16,40 @@ const getComputers = (setComputers) => {
     callApi(setComputers, null, config);
 }
 
-const onSearchChange = (setComputers, value) => {
-    if (value == "") {
-        getComputers(setComputers);
-    } else {
-        const config = {
-            method: "get",
-            endpoint: "computers/search/" + value
-        }
-
-        callApi(setComputers, null, config);
+const getRooms = (setRooms) => {
+    const config = {
+        method: "get",
+        endpoint: "rooms"
     }
+
+    callApi(setRooms, null, config);
+}
+
+const onSearchChange = (setComputers, computerCode, roomId) => {
+    const searchConfig = {
+        "computerCode": computerCode,
+        "roomId": roomId
+    }
+
+    const config = {
+        method: "post",
+        endpoint: "computers/search",
+        data: searchConfig
+    }
+
+    callApi(setComputers, null, config);
 }
 
 export const SearchComputerPage = () => {
     const [computers, setComputers] = useState([]);
+    const [rooms, setRooms] = useState([]);
     const [search, setSearch] = useState("");
     const [updated, setUpdated] = useState(true);
+    const [roomId, setRoomId] = useState("%%");
 
     useEffect(() => {
         getComputers(setComputers);
+        getRooms(setRooms);
     }, []);
 
     useEffect(() => {
@@ -48,32 +62,49 @@ export const SearchComputerPage = () => {
             <div className='dashBoardPadding'>
                 <h1>Computers</h1>
                 <OutlinedInput
-					id="search"
-					size="small"
-					placeholder={'Search By Code'}
+                    id="search"
+                    size="small"
+                    placeholder={'Search By Code'}
                     value={search}
-					startAdornment={
-						<InputAdornment position="start">
-							<SearchIcon />
-						</InputAdornment>
-					}
-					sx={{ borderRadius: 5 }}
+                    startAdornment={
+                        <InputAdornment position="start">
+                            <SearchIcon />
+                        </InputAdornment>
+                    }
+                    sx={{ borderRadius: 5 }}
                     onChange={(e) => {
                         if (/^\d+$/.test(e.target.value) || e.target.value == "") {
                             setSearch(e.target.value);
-                            onSearchChange(setComputers, e.target.value)
+                            onSearchChange(setComputers, e.target.value, roomId);
                         }
                     }}
-				/>
-                <div style={{display: 'flex', flexWrap: 'wrap'}}>
+                />
+                <InputLabel id="search-by-room">Search By Room</InputLabel>
+                <Select
+                    labelId='search-by-room'
+                    value={roomId}
+                    label="Search By Room"
+                    onChange={(e) => {
+                        setRoomId(e.target.value);
+                        onSearchChange(setComputers, search, e.target.value);
+                    }}
+                >
+                    <MenuItem value="%%">None</MenuItem>
+                    {
+                        rooms.map(room =>
+                            <MenuItem value={room.roomId} key={room.roomId}>{room.name}, {room.location.name}</MenuItem>
+                        )
+                    }
+                </Select>
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
                     {
                         computers.map(computer =>
-                            <ComputerCard computer = {computer} key = {computer.computerId}/>    
+                            <ComputerCard computer={computer} key={computer.computerId} />
                         )
                     }
                 </div>
             </div>
-            <AddComputer updated={[updated, setUpdated]}/>
+            <AddComputer updated={[updated, setUpdated]} />
         </>
     )
 }
