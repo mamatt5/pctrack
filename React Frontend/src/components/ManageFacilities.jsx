1ff29f2d80aa143f7b0436081ad2191bb84bfdd4
Kelly Xu <kelly.xu@fdmgroup.com>
2024-03-08 14:02:25 +1100

    fix in manage facilities

--- React Frontend/src/components/ManageFacilities.jsx
+++ React Frontend/src/components/ManageFacilities.jsx
@@ -1,14 +1,26 @@
-import AddLocationPage from "./addLocationPage"
-import AddRoomPage from "./addRoomPage"
+import AddLocationPage from "./addLocationPage";
+import AddRoomPage from "./addRoomPage";
 
-const ManageFacilities = ({admin, currStaff}) => {
-    console.log(admin)
-    console.log(currStaff)
-    return (<>
-    <AddLocationPage admin={admin}/>
-    <AddRoomPage admin={admin} currStaff={currStaff}/>
+const ManageFacilities = ({ admin, currStaff }) => {
+  console.log(admin);
+  console.log(currStaff);
+  const BusinessPerms = currStaff.filter((staff) => staff.adminLevel.name === "Business");
+  const LocationPerms = currStaff.filter((staff) => staff.adminLevel.name === "Location");
 
-    </>)
-}
+  return (
+    <>
+      {BusinessPerms.length !== 0 ? (
+        <>
+          <AddLocationPage admin={admin} />
+          <AddRoomPage admin={admin} currStaff={currStaff} />
+        </>
+      ) : BusinessPerms.length === 0 && LocationPerms.length !== 0 ? (
+        <>
+          <AddRoomPage admin={admin} currStaff={currStaff} />
+        </>
+      ) : null}
+    </>
+  );
+};
 
-export default ManageFacilities
+export default ManageFacilities;
